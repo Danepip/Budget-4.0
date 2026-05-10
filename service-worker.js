9077e69ac76beee815bbe73d4201a4691cc4e90f
Danepip <danoupascal.pierre@gmail.com>
2026-05-10 18:11:03 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "budget-card-view-v99";
+const VERSION = "budget-card-view-v100";
 const SHELL_CACHE = `${VERSION}-shell`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const APP_SHELL = [
